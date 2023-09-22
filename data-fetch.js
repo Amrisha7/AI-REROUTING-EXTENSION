ddef510ebe7f9f75afda7c21f2ed72be2144c054
Amrisha Mishra <136724257+Amrisha7@users.noreply.github.com>
2023-09-22 21:40:17 +0530

    Delete data-fetch.js

--- data-fetch.js
+++
@@ -1,8 +0,0 @@
-fetch('your-data-endpoint')
-  .then(response => response.json())
-  .then(data => {
-    // Process and update your chart with fetched data
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
